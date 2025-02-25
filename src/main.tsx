21b1a38bdd684c34671fb0efec2c6c9df956e94f
Kelsey Niederer <kelseyniederer@Kelseys-MBP.attlocal.net>
2025-02-25 07:32:50 -0600

    implement light mode

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,15 @@
+import { ChakraProvider, defaultSystem } from "@chakra-ui/react";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { Provider } from "./components/ui/provider";
+import { ColorModeProvider } from "./components/ui/color-mode";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <Provider>
-      <App />
-    </Provider>
+    <ChakraProvider value={defaultSystem}>
+      <ColorModeProvider forcedTheme="light">
+        <App />
+      </ColorModeProvider>
+    </ChakraProvider>
   </React.StrictMode>
 );
