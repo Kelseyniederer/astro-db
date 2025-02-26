836388472a138502362770d6aea67c641bac2fed
Kelsey Niederer <kelseyniederer@Kelseys-MBP.attlocal.net>
2025-02-26 07:19:50 -0600

    building game cards

--- src/App.tsx
+++ src/App.tsx
@@ -13,10 +13,10 @@ function App() {
       <GridItem area="nav">
         <NavBar></NavBar>
       </GridItem>
-      <GridItem area="aside" bg="gold" display={{ base: "none", lg: "block" }}>
+      <GridItem area="aside" display={{ base: "none", lg: "block" }}>
         Aside
       </GridItem>
-      <GridItem area="main" bg="dodgerblue">
+      <GridItem area="main">
         <GameGrid />
       </GridItem>
     </Grid>
