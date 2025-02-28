5cc57f27f0004f51623e6fca6d07c5aa3ce5cd8d
Kelsey Niederer <kelseyniederer@Kelseys-MBP.attlocal.net>
2025-02-27 20:29:13 -0600

    add genre list endpoint

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { Grid, GridItem } from "@chakra-ui/react";
 import GameGrid from "./components/GameGrid";
+import GenreList from "./components/GenreList";
 import NavBar from "./components/NavBar";
 
 function App() {
@@ -14,7 +15,7 @@ function App() {
         <NavBar></NavBar>
       </GridItem>
       <GridItem area="aside" display={{ base: "none", lg: "block" }}>
-        Aside
+        <GenreList />
       </GridItem>
       <GridItem area="main">
         <GameGrid />
