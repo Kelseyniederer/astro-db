5fad00eb1b53fa279b30d0105d06ffe7e7adfb51
Kelsey Niederer <kelseyniederer@Kelseys-MBP.attlocal.net>
2025-03-01 14:57:04 -0600

    display genres

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -5,23 +5,19 @@ import GameCardContainer from "./gameCardContainer";
 import GameCardSkeleton from "./gameCardSkeleton";
 
 const GameGrid = () => {
-  const { games, error, isLoading } = useGames();
+  const { data, error, isLoading } = useGames();
   const skeletons = [1, 2, 3, 4, 5, 6, 7, 8];
   return (
     <>
       {error && <Text>{error}</Text>}
-      <SimpleGrid
-        columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
-        padding={10}
-        gap={10}
-      >
+      <SimpleGrid columns={{ sm: 1, md: 2, lg: 3, xl: 4 }} padding={10} gap={3}>
         {isLoading &&
           skeletons.map((skeleton) => (
             <GameCardContainer>
               <GameCardSkeleton key={skeleton} />
             </GameCardContainer>
           ))}
-        {games.map((game) => (
+        {data.map((game) => (
           <GameCardContainer>
             <GameCard key={game.id} game={game}></GameCard>
           </GameCardContainer>
