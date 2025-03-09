79d8422465df01e1b479f82e217df5d7a45ebd22
Kelsey Niederer <kelseyniederer@Kelseys-MBP.attlocal.net>
2025-03-08 21:41:07 -0600

    updating logo

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,16 +1,24 @@
-import { HStack, Image } from "@chakra-ui/react";
+import { HStack } from "@chakra-ui/react";
 import { Link } from "react-router-dom";
-import logo from "../assets/logo.webp";
 import ColorModeSwitch from "./ColorModeSwitch";
+import Logo from "./Logo";
 import SearchInput from "./SearchInput";
 
 const NavBar = () => {
   return (
     <HStack padding="10px">
       <Link to="/">
-        <Image src={logo} boxSize="60px" objectFit="cover" />
+        <Logo
+          resetQuery={function (): void {
+            throw new Error("Function not implemented.");
+          }}
+        />
       </Link>
-      <SearchInput />
+      <SearchInput
+        onSearch={function (): void {
+          throw new Error("Function not implemented.");
+        }}
+      />
       <Link
         to="/natal-chart"
         style={{ marginLeft: "auto", marginRight: "20px" }}
