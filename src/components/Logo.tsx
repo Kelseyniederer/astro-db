1b8a976102df9a5c280774dc758065aad3528a19
Kelsey Niederer <kelseyniederer@Kelseys-MacBook-Pro.local>
2025-03-24 20:41:29 -0500

    fixed the nav bar

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -16,8 +16,8 @@ const Logo = ({ resetQuery }: Props) => {
   };
 
   return (
-    <HStack cursor="pointer" onClick={handleClick} spacing={2} pl={4} ml={0}>
-      <Icon as={BiCameraMovie} boxSize={10} color="white.400" />
+    <HStack cursor="pointer" onClick={handleClick}>
+      <Icon as={BiCameraMovie} boxSize={8} color="white.400" />
       <Text
         fontSize="2xl"
         fontWeight="bold"
@@ -26,7 +26,7 @@ const Logo = ({ resetQuery }: Props) => {
       >
         AstroDB
       </Text>
-      <Icon as={PiPlanet} boxSize={8} color="white.400" />
+      <Icon as={PiPlanet} boxSize={6} color="white.400" />
     </HStack>
   );
 };
