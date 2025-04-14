253a390ba41b29a33c4955b917d3be692b4a21b8
Kelsey Niederer <kelseyniederer@Kelseys-MBP.attlocal.net>
2025-04-14 14:17:33 -0500

    fixed libra date range

--- src/hooks/useZodiacSign.ts
+++ src/hooks/useZodiacSign.ts
@@ -8,8 +8,8 @@ const getZodiacSign = (month: number, day: number): string => {
   if ((month === 6 && day >= 21) || (month === 7 && day <= 22)) return "Cancer";
   if ((month === 7 && day >= 23) || (month === 8 && day <= 22)) return "Leo";
   if ((month === 8 && day >= 23) || (month === 9 && day <= 22)) return "Virgo";
-  if ((month === 9 && day >= 23) || (month === 10 && day <= 22)) return "Libra";
-  if ((month === 10 && day >= 23) || (month === 11 && day <= 21))
+  if ((month === 9 && day >= 23) || (month === 10 && day <= 23)) return "Libra";
+  if ((month === 10 && day >= 24) || (month === 11 && day <= 21))
     return "Scorpio";
   if ((month === 11 && day >= 22) || (month === 12 && day <= 21))
     return "Sagittarius";
